a2db236adef77c21a6929eef9ec274db8614c762
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2020-12-03 17:40:35 +0700

    commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    //[SerializeField]
+    private float _speed = 20f;
     [SerializeField]
-    private float _speed = 1.5f;
+    private GameObject laser;
 
     void Start()
     {
@@ -16,11 +18,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+	    InputMovement();
+        BoundingMovement();
+        SpawnLaser();
+    }
+
+    private void InputMovement()
+    {
 	    var horizontalInput = Input.GetAxis("Horizontal");
 	    var verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * _speed);
-        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * _speed);
+	    var direction = new Vector3(horizontalInput, verticalInput, 0);
+	    transform.Translate(direction * Time.deltaTime * _speed);
+    }
+    private void BoundingMovement()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+        var rightBound = 12.5f;
+        var leftBound = -12.5f;
+        if (transform.position.x >= rightBound)
+        {
+	        transform.position = new Vector3(leftBound, transform.position.y, 0);
+        } else if (transform.position.x <= leftBound)
+        {
+	        transform.position = new Vector3(rightBound, transform.position.y, 0);
+        }
+    }
 
-        //transform.position.x =
+    private void SpawnLaser()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+		    Instantiate(laser, transform.position, Quaternion.identity);
+	    }
     }
 }
